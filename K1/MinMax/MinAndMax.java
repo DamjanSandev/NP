307daa26506f4b85a4dc617b4c2b3fb845622d32
damjan-sandev <132223437+damjan-sandev@users.noreply.github.com>
2023-11-30 01:20:42 +0100

    Advanced Programming prob

--- K1/MinMax/MinAndMax.java
+++ K1/MinMax/MinAndMax.java
@@ -1,4 +1,61 @@
 package Napredno.K1.MinMax;
 
+import java.util.Scanner;
+
 public class MinAndMax {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        MinMax<String> strings = new MinMax<String>();
+        for(int i = 0; i < n; ++i) {
+            String s = scanner.next();
+            strings.update(s);
+        }
+        System.out.println(strings);
+        MinMax<Integer> ints = new MinMax<Integer>();
+        for(int i = 0; i < n; ++i) {
+            int x = scanner.nextInt();
+            ints.update(x);
+        }
+        System.out.println(ints);
+    }
+}
+
+class MinMax<T extends Comparable<T>>{
+    private T min,max;
+    private int countmax,coutmin,updates;
+    public MinMax() {
+        countmax=coutmin=updates=0;
+    }
+    public void update(T element){
+        if(updates==0){
+            min=max=element;
+        }
+        if(min.compareTo(element)>0){
+            min=element;
+            coutmin=1;
+        }
+        else if(min.compareTo(element)==0){
+            coutmin++;
+        }
+        if(max.compareTo(element)<0){
+            max=element;
+            countmax=1;
+        }
+        else if(max.compareTo(element)==0){
+            countmax++;
+        }
+        updates++;
+    }
+    public T max(){
+        return max;
+    }
+    public T min(){
+        return min;
+    }
+
+    @Override
+    public String toString() {
+        return min + " " + max + " " + (updates-countmax-coutmin) + "\n";
+    }
 }
