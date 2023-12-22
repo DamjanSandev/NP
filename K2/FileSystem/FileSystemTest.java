875083ca755b9ec2593a529481dfb0d49ac8cb6b
damjan-sandev <132223437+damjan-sandev@users.noreply.github.com>
2023-12-22 12:02:36 +0100

    Advanced Programming prob

---
+++ K2/FileSystem/FileSystemTest.java
@@ -0,0 +1,141 @@
+package Napredno.K2.FileSystem;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Partial exam II 2016/2017
+ */
+public class FileSystemTest {
+    public static void main(String[] args) {
+        FileSystem fileSystem = new FileSystem();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < n; i++) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(":");
+            fileSystem.addFile(parts[0].charAt(0), parts[1],
+                    Integer.parseInt(parts[2]),
+                    LocalDateTime.of(2016, 12, 29, 0, 0, 0).minusDays(Integer.parseInt(parts[3]))
+            );
+        }
+        int action = scanner.nextInt();
+        if (action == 0) {
+            scanner.nextLine();
+            int size = scanner.nextInt();
+            System.out.println("== Find all hidden files with size less then " + size);
+            List<File> files = fileSystem.findAllHiddenFilesWithSizeLessThen(size);
+            files.forEach(System.out::println);
+        } else if (action == 1) {
+            scanner.nextLine();
+            String[] parts = scanner.nextLine().split(":");
+            System.out.println("== Total size of files from folders: " + Arrays.toString(parts));
+            int totalSize = fileSystem.totalSizeOfFilesFromFolders(Arrays.stream(parts)
+                    .map(s -> s.charAt(0))
+                    .collect(Collectors.toList()));
+            System.out.println(totalSize);
+        } else if (action == 2) {
+            System.out.println("== Files by year");
+            Map<Integer, Set<File>> byYear = fileSystem.byYear();
+            byYear.keySet().stream().sorted()
+                    .forEach(key -> {
+                        System.out.printf("Year: %d\n", key);
+                        Set<File> files = byYear.get(key);
+                        files.stream()
+                                .sorted()
+                                .forEach(System.out::println);
+                    });
+        } else if (action == 3) {
+            System.out.println("== Size by month and day");
+            Map<String, Long> byMonthAndDay = fileSystem.sizeByMonthAndDay();
+            byMonthAndDay.keySet().stream().sorted()
+                    .forEach(key -> System.out.printf("%s -> %d\n", key, byMonthAndDay.get(key)));
+        }
+        scanner.close();
+    }
+}
+
+// Your code here
+
+class File implements Comparable<File>{
+    private final String name;
+    private final int size;
+    private final LocalDateTime dateTime;
+
+    public File(String name, int size, LocalDateTime dateTime) {
+        this.name = name;
+        this.size = size;
+        this.dateTime = dateTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    @Override
+    public int compareTo(File o) {
+        Comparator<File> comparator=Comparator.comparing(File::getDateTime).thenComparing(File::getName).thenComparing(File::getSize);
+        return comparator.compare(this,o);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-10s %5dB %s",name,size,dateTime.toString());
+    }
+}
+class FileSystem{
+     private final Map<Character,Set<File>> filesByFolder;
+     public FileSystem(){
+         filesByFolder=new HashMap<>();
+     }
+    public void addFile(char c, String part, int i, LocalDateTime localDateTime) {
+         if(!filesByFolder.containsKey(c)){
+             filesByFolder.put(c,new TreeSet<>());
+         }
+         filesByFolder.get(c).add(new File(part,i,localDateTime));
+    }
+
+    public List<File> findAllHiddenFilesWithSizeLessThen(int size) {
+        return filesByFolder.values().stream()
+                .flatMap(Set::stream)
+                .filter(file -> file.getName().startsWith(".") && file.getSize() < size)
+                .collect(Collectors.toList());
+    }
+
+    public int totalSizeOfFilesFromFolders(List<Character> folders) {
+        return folders.stream()
+                .flatMap(folder -> filesByFolder.getOrDefault(folder, Collections.emptySet()).stream())
+                .mapToInt(File::getSize)
+                .sum();
+    }
+
+    public Map<Integer, Set<File>> byYear() {
+        return filesByFolder.values().stream()
+                .flatMap(Set::stream)
+                .collect(Collectors.groupingBy(
+                        file -> file.getDateTime().getYear(),
+                        Collectors.toSet()
+                ));
+        }
+    public Map<String, Long> sizeByMonthAndDay() {
+        return filesByFolder.values().stream()
+                .flatMap(Set::stream)
+                .collect(Collectors.groupingBy(
+                        file -> getMonthAndDayKey(file.getDateTime()),
+                        Collectors.summingLong(File::getSize)
+                ));
+    }
+    private String getMonthAndDayKey(LocalDateTime dateTime) {
+
+        return String.format("%s-%d",dateTime.getMonth().toString(),dateTime.getDayOfMonth());
+    }
+}
